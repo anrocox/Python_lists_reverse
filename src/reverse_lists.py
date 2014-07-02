982874023de5929ab88b1e34aa7bd354bbffb824
Andres Rodriguez <anrocox@gmail.com>
2014-07-02 10:27:33 -0500

    I have a list in python and I want to invest the elements, ie the latter is the first, how I can do?

---
+++ src/reverse_lists.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+'''
+Created on Jul 1, 2014
+
+@author: anroco
+
+I have a list in python and I want to invest the elements, ie the latter is the
+first, how I can do?
+
+Tengo una lista en python y quiero invertir los elementos de la lista, es
+decir que el último sea el primero, ¿como puedo hacerlo?
+'''
+
+#create a list
+lista = [9, 2, 5, 10, 9, 1, 3]
+print (lista)
+
+#reverses the order of the list
+lista.reverse()
+print (lista)
+
+#create a list
+lista = ['abc', 'a', 'bcd', 'c', 'bb', 'abcd']
+print (lista)
+
+#reverses the order of the list
+lista.reverse()
+print (lista)
